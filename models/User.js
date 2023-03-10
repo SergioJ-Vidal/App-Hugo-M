b6c3a3ce60bc1f19998d2a02c9aa6b8acb577154
SergioJ-Vidal <113844469+SergioJ-Vidal@users.noreply.github.com>
2023-03-10 15:49:28 +0100

    Edad usuario y fecha nacimiento

--- models/User.js
+++ models/User.js
@@ -2,17 +2,39 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
 
-name: String,
-
-surname: String,
-
-password: String,
-
-age: Number,
-
-email: String,
+    Nombre: {
+        type: String,
+        required: true
+    },
+    Apellidos: {
+        type: String,
+        required: true
+    },
+    Edad: {
+        type: Number,
+    },
+    Email: {
+        type: String,
+        required: true
+    },
+    Password: {
+        type: String,
+        required: true
+    },
+    Fecha_nacimiento: {
+        type: Date,
+        required: true
+    }
 
 }, { timestamps: true });
+
+UserSchema.pre('save', function (next) {
+    const fecha_nacimiento = this.Fecha_nacimiento;
+    const edad = Math.floor((Date.now() - fecha_nacimiento.getTime()) / (1000 * 60 * 60 * 24 * 365));
+    console.log(edad)
+    this.Edad = edad;
+    next();
+});
 
 const User = mongoose.model('User', UserSchema);
 
