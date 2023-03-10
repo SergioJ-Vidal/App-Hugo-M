adb868cb3f4065c62dd86f3e7ae01ac63e3c19cb
SergioJ-Vidal <113844469+SergioJ-Vidal@users.noreply.github.com>
2023-03-10 11:58:14 +0100

    User first

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+
+name: String,
+
+surname: String,
+
+age: Number,
+
+email: String,
+
+
+
+}, { timestamps: true });
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
